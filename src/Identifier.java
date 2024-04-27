e0a1ada76952dc92efbfdef47d5df25696b015f0
Katharina Gråman <katharinagramanswe@gmail.com>
2024-04-27 18:01:57 +0100

    Identifier for an event

--- src/Identifier.java
+++ src/Identifier.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Identifier {
+public class Identifier {
+    private String task, event, chore, other;
+    private String id;
+
+
+    private String getId(){
+        return id;
+
+    }
+    private void setId(String identifier){
+        switch (identifier){
+            case "task":
+                id = "task";
+            case "event":
+                id = "event";
+            case "chore":
+                id = "chore";
+            default:
+                id = "other";
+        }
+
+
+    }
 }
