42888aa5be234d74973185d0b2b9e8ebae787d9c
Katharina Gråman <katharinagramanswe@gmail.com>
2024-04-30 16:24:26 +0100

    added two textfields to create a string of time

--- src/EventGUI.java
+++ src/EventGUI.java
@@ -26,8 +26,10 @@ public class EventGUI extends JFrame {
     private LocalTime userInputedTime;
     private DailyEvent userInputedEvent;
 
+    private String[] times = new String[24];
+
     private JPanel titleOfEventPanel, typeOfEventPanel, startAndEndPanel, descriptionPanel;
-    private JTextField titleOfEvent, typeOfEvent, startAndEnd; // typeOfevent ska va en såndär meny man markerar
+    private JTextField titleOfEvent, typeOfEvent, start1,start2, end1, end2; // typeOfevent ska va en såndär meny man markerar
     //default ska vara en task
     private JTextField[] arrayOfNeccText = new JTextField[3];
     private JFrame taskFrame = new JFrame();
@@ -40,6 +42,10 @@ public class EventGUI extends JFrame {
 
 
     public EventGUI(EventListener day) {
+        for(int i = 0; i<24; i++){
+            this.times[i] = String.valueOf(i);
+
+        }
         this.eventListener = day;
 
 
@@ -92,23 +98,33 @@ public class EventGUI extends JFrame {
         typeComboBox = new JComboBox<>(eventTypeOptions);
         mainPanel.add(typeComboBox, gbc);
 
-
         gbc.gridx = 0;
         gbc.gridy = 2;
         mainPanel.add(start, gbc);
         gbc.gridx = 1;
-        startAndEnd = new JTextField(20); // Initialize text field
-        mainPanel.add(startAndEnd, gbc);
+        start1 = new JTextField(5); // Initialize text field
+        mainPanel.add(start1, gbc);
+        gbc.gridx = 2;
+        start2 = new JTextField(5);
+        mainPanel.add(start2, gbc);
 
         gbc.gridx = 0;
-        gbc.gridy =3;
+        gbc.gridy = 3;
+        mainPanel.add(end, gbc);
+        gbc.gridx = 1;
+        end1 = new JTextField(5);
+        mainPanel.add(end1,gbc);
+        gbc.gridx = 2;
+        end2 = new JTextField(5);
+        mainPanel.add(end2, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
         JLabel descriptionOfEvent = new JLabel("Description: ");
         mainPanel.add(descriptionOfEvent, gbc);
         gbc.gridx = 1;
         description = new JTextArea(10,20);
         mainPanel.add(description, gbc);
-
-
 
 
         // Add mainPanel to CENTER of taskFrame content pane
@@ -131,7 +147,7 @@ public class EventGUI extends JFrame {
                  * Jag vill egentligen att allt ska nollstälals så man kan fortsätta till man är nöjd. Och när den stängs så stängs inte allt
                  * Bara det fönstret där man lägger till
                  */
-                if (!titleOfEvent.getText().isEmpty() && !startAndEnd.getText().isEmpty()) {
+                if (!titleOfEvent.getText().isEmpty() && !start1.getText().isEmpty() && !start2.getText().isEmpty() && !end1.getText().isEmpty() && !end2.getText().isEmpty()) {
                     System.out.println("hej"); //printar så denna funkar men den printar alltid när jah trycker tyvärr
                     addEvent(day);
                     dispose();
@@ -155,7 +171,8 @@ public class EventGUI extends JFrame {
     public void addEvent(EventListener day){
         String title = titleOfEvent.getText();
         String type = typeComboBox.getItemAt(typeComboBox.getSelectedIndex());
-        String start = startAndEnd.getText();
+        String start = getStartTime(start1,start2);
+        String end = getEndTime(end1,end2);
         String descriptionE = description.getText();
         DailyEvent newEvent = new DailyEvent(title, type, start,start, descriptionE);
         // Parse the inputed start and end time
@@ -169,7 +186,37 @@ public class EventGUI extends JFrame {
          */
 
         tasksThisDay.put(time, newEvent);
-        day.onEventAdded(title, type, start,start, descriptionE);
+        day.onEventAdded(title, type, start,end, descriptionE);
+
+    }
+    /**
+     * Gör så att den översätter strängar till LocalTime ist
+     */
+    public String getStartTime(JTextField a, JTextField b){
+        if(legalInputTimes(a.getText()) && legalInputTimes(b.getText())){
+            return a.getText() +":"+b.getText();
+        }
+        return null;
+
+    }
+
+    public String getEndTime(JTextField a, JTextField b){
+        if(legalInputTimes(a.getText()) && legalInputTimes(b.getText())){
+            return a.getText() +":"+b.getText();
+        }
+        return null;
+
+    }
+
+    public boolean legalInputTimes(String a){
+        for(String x : times){
+            if(x.equals(a)){
+                return true;
+            }
+        }
+        return false;
+
+
 
     }
 
